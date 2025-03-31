da3fe4059480b92418783704d51782c8a06f62f7
Alessio Gebaila <gebailaalessio22@stud.ase.ro>
2025-03-31 18:42:04 +0300

    the final version of the application

--- src/ase/en/sqt/main/Main.java
+++ src/ase/en/sqt/main/Main.java
@@ -1,7 +1,15 @@
 package ase.en.sqt.main;
 
+import ase.en.sqt.cli.CliManager;
+import ase.en.sqt.manager.DispatcherManager;
+import ase.en.sqt.sheriff.Sheriff;
+
 public class Main {
     public static void main(String[] args) {
+        Sheriff sheriff = null;
+        DispatcherManager dispatcherManager = new DispatcherManager();
+        CliManager cliManager = new CliManager(sheriff, dispatcherManager);
 
+        cliManager.start();
     }
 }
